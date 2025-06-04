0c6aa088aa53928620fee30abc6666097b7dede9
gyoyeon Kim <gy2121@naver.com>
2025-06-04 13:01:29 +0900

    week1

--- vscode/Week1/pb2.js
+++ vscode/Week1/pb2.js
@@ -1,4 +1,4 @@
-/* 숫자 비교하기 */
+/* 숫자 비교하기 - 삼항연산자 사용 */
 function soultion(num1, num2) {
   var answer = num1 === num2 ? 1 : -1;
   return answer;
--- vscode/Week1/pb3.js
+++ vscode/Week1/pb3.js
@@ -1,3 +1,5 @@
+/* 특정 문자 제거하기 - replaceAll*/
+
 function solution(my_string, letter) {
   return my_string.replaceAll(letter, "");
 }
